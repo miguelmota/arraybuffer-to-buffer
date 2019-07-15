6d8e42cb5e550cbaa14fbadfdf71a89ba7af3b9b
Blaine <blaine.mcgary@pnnl.gov>
2019-07-15 11:23:24 -0700

    Updated buffer instantiation to not use deprecated constructor

--- arraybuffer-to-buffer.js
+++ arraybuffer-to-buffer.js
@@ -1,14 +1,14 @@
 (function(root) {
-  var isArrayBufferSupported = (new Buffer(new Uint8Array([1]).buffer)[0] === 1);
+  var isArrayBufferSupported = (Buffer.from(new Uint8Array([1]).buffer)[0] === 1);
 
   var arrayBufferToBuffer = isArrayBufferSupported ? arrayBufferToBufferAsArgument : arrayBufferToBufferCycle;
 
   function arrayBufferToBufferAsArgument(ab) {
-    return new Buffer(ab);
+    return Buffer.from(ab);
   }
 
   function arrayBufferToBufferCycle(ab) {
-    var buffer = new Buffer(ab.byteLength);
+    var buffer = Buffer.alloc(ab.byteLength);
     var view = new Uint8Array(ab);
     for (var i = 0; i < buffer.length; ++i) {
         buffer[i] = view[i];
